d57de948bc48c753bc013aabd5a88f07d29ebbc9
confirmeyes <confirmeyes@163.com>
2020-06-06 08:57:25 +0800

    。

--- free-web/src/main/java/model/User.java
+++ free-web/src/main/java/model/User.java
@@ -10,9 +10,7 @@ import lombok.Setter;
  * @description .
  */
 
-@Getter
-@Setter
-@AllArgsConstructor
+
 public class User {
 
     private Integer id;
@@ -20,5 +18,42 @@ public class User {
     private Integer age;
     private String gender;
 
+    public User(Integer id, String name, Integer age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
